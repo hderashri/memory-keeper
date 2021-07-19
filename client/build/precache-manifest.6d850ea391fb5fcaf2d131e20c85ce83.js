22bb6aa718f3bb7ba853bf4ee8628208149e8121
Rajita Roy <royrajita6@gmail.com>
2021-07-19 21:00:11 +0530

    deployed

---
+++ client/build/precache-manifest.6d850ea391fb5fcaf2d131e20c85ce83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e42feda439bbb2876cc9e4b12c85fa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccc322e7eb98bc7da65b",
+    "url": "/static/css/main.fb0789c2.chunk.css"
+  },
+  {
+    "revision": "c0760040102295bd88f5",
+    "url": "/static/js/2.22f9ae28.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.22f9ae28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc322e7eb98bc7da65b",
+    "url": "/static/js/main.26706d77.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
